759a895e4377319612d90af19a74e4e10d950758
TaikiTkayanagi <tynagi@icloud.com>
2025-08-12 09:40:32 +0900

    add: first commit

---
+++ app/src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+
+public class Main {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+    }
+}
