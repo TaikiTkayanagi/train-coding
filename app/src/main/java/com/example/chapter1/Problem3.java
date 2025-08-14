6a2918f84e210ebe9d5648f0223b20056523685e
TaikiTkayanagi <tynagi@icloud.com>
2025-08-14 14:38:58 +0000

    change: inputの条件を変更

--- app/src/main/java/com/example/chapter1/Problem3.java
+++ app/src/main/java/com/example/chapter1/Problem3.java
@@ -2,16 +2,29 @@ package com.example.chapter1;
 
 public class Problem3 {
     public static void main(String[] args) {
-        String input = "Mr Jhon Smith";
-        int length = input.length();
-        System.out.println("input: " + input);
-        System.out.println("length: " + length);
-
+        char[] input = "Mr Jhon Smith    ".toCharArray();
+        int length = 13;
+        int spaceCount = 0;
         for (int i = 0; i < length; i++) {
-            if (input.charAt(i) == ' ') {
-                input = input.substring(0, i) + "%20" + input.substring(i + 1);
+            if (input[i] == ' ')
+                spaceCount++;
+        }
+
+        int newLength = length + spaceCount * 2;
+        for (int i = length - 1; i >= 0; i--) {
+            if (input[i] == ' ') {
+                input[newLength - 1] = '0';
+                input[newLength - 2] = '2';
+                input[newLength - 3] = '%';
+                newLength -= 3;
+            } else {
+                input[newLength - 1] = input[i];
+                newLength--;
             }
         }
-        System.out.println(input);
+
+        for (var v : input) {
+            System.out.print(v);
+        }
     }
 }
